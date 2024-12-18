6284cc709738825737af1a6511eb9b99c8c9c06b
Heini Järviö <heinimjarv@gmail.com>
2024-12-18 20:14:11 +0100

    parsing code completed

--- reading_html.py
+++ reading_html.py
@@ -1,8 +1,29 @@
 import pandas as pd
 from bs4 import BeautifulSoup
 import os
+import pickle
 
+"""
+This code reads the previously stored "scraped_page" html-file 
+that contains the information of a student's course selection. This file contains the necessary information
+in a set of tables, where one table contains the information from one course.
+
+The beautifulsoup library is used to read the file into a list that is then 
+combined into a dictionary object for easier future use. This dictionary is 
+then stored as a binary object using the pickle module.
+
+Author: Heini Järviö
+
+"""
+
+# Defining the location of the working directory
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+
+# File path to the stored html file
+html_file_path = os.path.join(__location__, "data/scraped_page.htm")
+
+# A function that is used to create a dictionary of a table in the data by combining
+# the information from first index of the list with information from the second index
 
 
 def table_to_dict(table):
@@ -12,8 +33,11 @@ def table_to_dict(table):
     return dictionary
 
 
+# A function to parse data from the stored html file
+
+
 def read_html_with_beautiful_soup(file_path):
-    # Read HTML file
+    # Read HTML file, making sure encoding is utf-8
     with open(file_path, encoding="utf-8") as f:
         # Parse HTML using BeautifulSoup
         soup = BeautifulSoup(f, "html.parser")
@@ -30,6 +54,8 @@ def read_html_with_beautiful_soup(file_path):
         if df.shape[1] == 2:
             dfs_with_two_cols.append(df)
 
+    # Using the earlier defined function, we combine all tables into a nested dictionary
+
     dicts = []
     for df in dfs_with_two_cols:
         dicts.append(table_to_dict(df))
@@ -37,11 +63,27 @@ def read_html_with_beautiful_soup(file_path):
     return dicts
 
 
-# File path
-html_file_path = os.path.join(__location__, "data/scraped_page.htm")
-
 # Read HTML file using BeautifulSoup with read_html()
 tables = read_html_with_beautiful_soup(html_file_path)
 
-# Display DataFrame
-print(tables)
+
+# Saving our dictionary of tables using pickle
+def save_tables(filename, tables):
+    with open(filename, "wb") as file:
+        pickle.dump(tables, file)
+
+
+# Defining the file name and location in the data-folder
+save_tables(os.path.join(__location__, "data/my-tables.pkl"), tables)
+
+
+# A function to load our data again; testing that it was stored correctly
+def load_tables(filename):
+    with open(filename, "rb") as file:
+        return pickle.load(file)
+
+
+loaded_tables = load_tables(os.path.join(__location__, "data/my-tables.pkl"))
+
+# Displaying the loaded dataframe
+print(loaded_tables)
