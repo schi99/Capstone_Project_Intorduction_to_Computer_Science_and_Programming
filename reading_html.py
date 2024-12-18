e5dcef831570eca24960fdd358c96729daf79de2
Heini Järviö <heinimjarv@gmail.com>
2024-12-18 19:44:06 +0100

    added code to create dict

--- reading_html.py
+++ reading_html.py
@@ -5,6 +5,13 @@ import os
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
 
+def table_to_dict(table):
+    keys = table.iloc[:, 0].tolist()
+    values = table.iloc[:, 1].tolist()
+    dictionary = dict(zip(keys, values))
+    return dictionary
+
+
 def read_html_with_beautiful_soup(file_path):
     # Read HTML file
     with open(file_path, encoding="utf-8") as f:
@@ -13,15 +20,28 @@ def read_html_with_beautiful_soup(file_path):
     # Find all tables in the HTML
     tables = soup.find_all("table")
     # Read tables into DataFrame using read_html()
-    df = pd.read_html(str(tables))[0]
-    return df
+    dfs = pd.read_html(str(tables))
+
+    # df.shape gives us the number of [rows, columns] in the dataframe.
+    # We check all of the dataframes in dfs and only keep the ones that
+    # have columns == 2.
+    dfs_with_two_cols = []
+    for df in dfs:
+        if df.shape[1] == 2:
+            dfs_with_two_cols.append(df)
+
+    dicts = []
+    for df in dfs_with_two_cols:
+        dicts.append(table_to_dict(df))
+
+    return dicts
 
 
 # File path
 html_file_path = os.path.join(__location__, "data/scraped_page.htm")
 
 # Read HTML file using BeautifulSoup with read_html()
-df = read_html_with_beautiful_soup(html_file_path)
+tables = read_html_with_beautiful_soup(html_file_path)
 
 # Display DataFrame
-print(df)
+print(tables)
