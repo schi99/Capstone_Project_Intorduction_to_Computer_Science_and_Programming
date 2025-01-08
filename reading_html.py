bcd5b78122b3d9e3f328b8676ade2d2c4e8ee4d5
Heini Järviö <heinimjarv@gmail.com>
2025-01-08 10:49:10 +0100

    updated deprecated code

--- reading_html.py
+++ reading_html.py
@@ -2,6 +2,7 @@ import pandas as pd
 from bs4 import BeautifulSoup
 import os
 import pickle
+from io import StringIO
 
 """
 This code reads the previously stored "scraped_page" html-file 
@@ -43,8 +44,11 @@ def read_html_with_beautiful_soup(file_path):
         soup = BeautifulSoup(f, "html.parser")
     # Find all tables in the HTML
     tables = soup.find_all("table")
+    # Convert the tables to a string and wrap it in a StringIO object
+    tables_str = str(tables)
+    tables_io = StringIO(tables_str)
     # Read tables into DataFrame using read_html()
-    dfs = pd.read_html(str(tables))
+    dfs = pd.read_html(tables_io)
 
     # df.shape gives us the number of [rows, columns] in the dataframe.
     # We check all of the dataframes in dfs and only keep the ones that
