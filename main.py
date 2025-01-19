db5f764b0a59ede242fda65bfcc50c6f1f0eac05
Heini Järviö <heinimjarv@gmail.com>
2025-01-19 18:31:27 +0100

    rewrote os filepath using pathlib

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from reading_html import read_html_with_beautiful_soup, save_tables
 from calendar_output import create_calendar, save_calendar
-import os
+from pathlib import Path
 
 """
 This code loads the downloaded HTML file, parses the data and
@@ -11,17 +11,15 @@ Author: Heini Järviö
 
 if __name__ == "__main__":
 
-    __location__ = os.path.realpath(
-        os.path.join(os.getcwd(), os.path.dirname(__file__))
-    )
+    __location__ = Path(__file__).resolve().parent
 
-    html_file_path = os.path.join(__location__, "data/uniportal.htm")
+    html_file_path = __location__ / "data/uniportal.htm"
 
     course_info = read_html_with_beautiful_soup(html_file_path)
 
     # Saving the tables for later use
-    save_tables(os.path.join(__location__, "data/my-tables.pkl"), course_info)
+    save_tables(__location__ / "data/my-tables.pkl", course_info)
 
     lessons = create_calendar(course_info)
 
-    save_calendar(lessons, os.path.join(__location__, "data/unical.ics"))
+    save_calendar(lessons, __location__ / "data/unical.ics")
